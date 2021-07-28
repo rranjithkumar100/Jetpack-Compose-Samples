66e5277c66eb1df8336e24b5699d21f7584c1a9b
Ranjithkumar <jetpackcompose.net@gmail.com>
2021-07-28 04:22:35 +0300

    text samples added

--- Compose Text/app/src/main/java/net/jetpackcompose/composetext/MainActivity.kt
+++ Compose Text/app/src/main/java/net/jetpackcompose/composetext/MainActivity.kt
@@ -1,15 +1,25 @@
 package net.jetpackcompose.composetext
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material.ListItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import net.jetpackcompose.composetext.ui.theme.ComposeTextTheme
+import net.jetpackcompose.composetext.ui.theme.Purple200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,22 +28,60 @@ class MainActivity : ComponentActivity() {
             ComposeTextTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    BigText()
+                    TextContainer()
                 }
             }
         }
     }
+
+}
+@Composable
+fun TextContainer() {
+    Column {
+        TextWithSize("Big text", 40.sp)
+        MaxLines()
+        ColorText()
+        BoldText()
+        ItalicText()
+        OverflowedText()
+        SelectableText()
+    }
+}
+@Composable
+fun TextWithSize(label: String, size: TextUnit) {
+    Text(label, fontSize = size)
+}
+@Composable
+fun ColorText() {
+    Text("Color Text", color = Purple200)
+}
+@Composable
+fun BoldText() {
+    Text("Bold text", fontWeight = FontWeight.Bold)
 }
 
 @Composable
-fun BigText() {
-    Text("Hello World", fontSize = 40.sp)
+fun ItalicText() {
+    Text("Italic Text", fontStyle = FontStyle.Italic)
 }
-
+@Composable
+fun MaxLines() {
+    Text("hello ".repeat(50), maxLines = 2)
+}
+@Composable
+fun OverflowedText() {
+    Text("Overflow text  ".repeat(50), maxLines = 2, overflow = TextOverflow.Ellipsis)
+}
+@Composable
+fun SelectableText() {
+    SelectionContainer {
+        Text("This text is selectable")
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTextTheme {
-        BigText()
+        TextContainer()
     }
 }
